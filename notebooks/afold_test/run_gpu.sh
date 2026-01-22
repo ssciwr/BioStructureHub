8201ea48540c01b498e0aeb3ee8746500edae1ed
ChristineSchulz <aq354@uni-heidelberg.de>
2026-01-22 16:40:49 +0100

    fixed AFold CI

--- notebooks/afold_test/run_gpu.sh
+++ notebooks/afold_test/run_gpu.sh
@@ -7,9 +7,9 @@ module load bio/alphafold/3.0.1
 
 # Run with option --norun_data_pipeline for featurisation and model inference
 python $ALPHAFOLD_BIN_DIR/run_alphafold.py \
-    --json_path=afold_test/output/test/test_data.json \
+    --json_path=/home/christine/Sandbox/BioStructureHub/notebooks/afold_test/output/test/test_data.json \
     --db_dir=$ALPHAFOLD_DATABASES \
-    --model_dir=af3models \
-    --output_dir=afold_test/output_gpu \
+    --model_dir=/home/christine/Sandbox/BioStructureHub/notebooks/af3models \
+    --output_dir=/home/christine/Sandbox/BioStructureHub/notebooks/afold_test/output_gpu \
     --norun_data_pipeline
 
