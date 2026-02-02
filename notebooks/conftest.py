c1fea78b77a638eaa3d8d0d55833ad3a27478c5a
ChristineSchulz <aq354@uni-heidelberg.de>
2026-02-02 16:20:49 +0100

    boltzgen conftest

--- notebooks/conftest.py
+++ notebooks/conftest.py
@@ -9,6 +9,7 @@ ALPHAFOLD_MODEL_DIR = Path.home() / "af3models"
 ALPHAFOLD_WORKING_DIR = Path.home() / "afold_test"  # must be created by user
 ALPHAFOLD_RESULTS_DIR_PART1 = ALPHAFOLD_WORKING_DIR / "output"
 BOLTZ_WORKING_DIR = Path.home() / "boltz_test"  # must be created by user
+BOLTZGEN_WORKING_DIR = Path.home() / "protein_design_w_Boltzgen"  # created by user
 RFDIFFUSION_WORKING_DIR = Path.home() / "protein_design_RFDiffusion"
 BINDCRAFT_WORKING_DIR = Path.home() / "protein_design_w_Bindcraft"
 
@@ -23,6 +24,7 @@ def pytest_sessionstart(session):
     ALPHAFOLD_RESULTS_DIR_PART1.mkdir(exist_ok=True)
 
     BOLTZ_WORKING_DIR.mkdir(exist_ok=True)
+    BOLTZGEN_WORKING_DIR.mkdir(exist_ok=True)
     RFDIFFUSION_WORKING_DIR.mkdir(exist_ok=True)
     BINDCRAFT_WORKING_DIR.mkdir(exist_ok=True)
 
@@ -35,6 +37,7 @@ def pytest_sessionfinish(session, exitstatus):
     # delete all the created dirs
     shutil.rmtree(ALPHAFOLD_MODEL_DIR)
     shutil.rmtree(RFDIFFUSION_WORKING_DIR)
+    shutil.rmtree(BOLTZGEN_WORKING_DIR)
     shutil.rmtree(BINDCRAFT_WORKING_DIR)
     # shutil.rmtree(ALPHAFOLD_WORKING_DIR)
     # shutil.rmtree(BOLTZ_WORKING_DIR)
